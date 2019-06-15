826f1b00f5c427b319b460f1a3a7bc022f9a202f
Dale Lim <ancient.mystic.wonder@gmail.com>
2019-06-15 12:08:04 +1000

    Added antd table

--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -1,30 +1,72 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Table, Tag } from "antd";
 
 const CompaniesTable = ({ companies, onCompanyClick }) => {
-  const companyItems = companies.map(company => (
-    <CompaniesTableItem key={company.id} onCompanyClick={onCompanyClick} company={company} />
-  ));
-  return <div>{companyItems}</div>;
-};
+  const dataSource = companies.map(company => ({
+    key: company.id,
+    id: company.id,
+    companyName: company.name,
+    city: company.city,
+    willSponsor: company.willSponsor
+  }));
 
-const CompaniesTableItem = props => (
-  <p
-    onClick={() => {
-      props.onCompanyClick(props.company);
-    }}
-  >
-    {props.company.name} {props.company.willSponsor}
-  </p>
-);
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "companyName",
+      key: "companyName"
+    },
+    {
+      title: "City",
+      dataIndex: "city",
+      key: "city"
+    },
+    {
+      title: "Will Sponsor?",
+      dataIndex: "willSponsor",
+      key: "willSponsor",
+      render: text => {
+        let color = "green";
+        switch (text) {
+          case "YES":
+            color = "green";
+            break;
+          case "NO":
+            color = "red";
+            break;
+          case "MAYBE":
+            color = "orange";
+            break;
+          default:
+        }
+
+        return (
+          <Tag color={color} key={text}>
+            {text.toUpperCase()}
+          </Tag>
+        );
+      }
+    }
+  ];
+
+  return (
+    <Table
+      columns={columns}
+      dataSource={dataSource}
+      onRow={(record, rowIndex) => {
+        return {
+          onClick: () => {
+            onCompanyClick(companies[rowIndex]);
+          }
+        };
+      }}
+    />
+  );
+};
 
 CompaniesTable.propTypes = {
   companies: PropTypes.array,
-  onCompanyClick: PropTypes.func.isRequired
-};
-
-CompaniesTableItem.propTypes = {
-  company: PropTypes.object.isRequired,
   onCompanyClick: PropTypes.func.isRequired
 };
 
